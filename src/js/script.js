1e9a6a59c7eac2bcfbf159fdffd111f0592ed021
Phonk05 <106838444+Phonk05@users.noreply.github.com>
2022-10-23 21:33:33 +0400

    23/10/22 21.40 почти всееее!!!

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,13 @@
+
+// querySelector() Функция, которая ищет элементы на странице.
+// addEventListener() Функция, которая слушает ваши действия над элементом.
+// classList Объект, внутри которого есть функции управляющие классами.
+// add()  является функцией добавляющей доп. класс.
+// Добавляет класс: searchElem.classList.add();
+// Удаляет класс: searchElem.classList.remove();
+// Если нет класса, добавляет, если есть, удаляет: searchElem.classList.toggle();
+// Сообщает, есть ли класс у элемента: searchElem.classList.contains();
+
 const popup = document.querySelector('.section__form');
 const buttonOpen = document.querySelector('.button__btn-js');
 const buttonClose = document.querySelector('.button-close');
@@ -15,4 +25,6 @@ window.addEventListener('keydown', function(event) {
         popup.classList.remove('open');
     }
 })
-  
+
+// фокус
+
